8c9deba098ff3e38e0bda2e650899a9ea25c8958
akash-vhotkar <akashvhotkar4@gmail.com>
2020-12-09 21:41:02 +0530

    message

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/styles', express.static('styles'))
+app.use(session({ secret: "Shh, its a secret!", resave: false }));
+const url = "mongodb+srv://akash:akash1234@cluster0.4ayge.mongodb.net/cart?retryWrites=true&w=majority";
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("mongo db connected"))
+    .catch((err) => console.log(err));
+
+app.use('/', require('./routes/authenticate'))
+
+app.use('/', require('./routes/cart'))
+
+app.listen(5000, () => console.log("Server Running..."));
